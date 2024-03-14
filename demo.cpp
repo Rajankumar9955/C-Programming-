75480da4db1710d6aeab2daef41859559889d427
Rajan <krajan92946@gmail.com>
2024-03-14 12:57:57 +0530

    Rajan

---
+++ demo.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+{
+    int a,b;
+    cout<<" Enter the first number: ";
+    cin>>a;
+    cout<<" Enter the second number:";
+    cin>>b;
+    int c=a+b;
+    cout<<"sum is: "<<c;
+    return 0;
+}
---
+++ print.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int a=5*8-2*2;
+    printf("%d\n",a);
+    return 0;
+}
